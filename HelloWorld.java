88691281bf26166d6abb6fa46cf063a0fe698961
Rashi Jain <36439787+rashi-jain28@users.noreply.github.com>
2020-08-25 17:06:40 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("---*****----------- Hello World and welcome to Jenkins Training next part");
+		System.out.println("--Trying Webhook---------");
 	}
 }
